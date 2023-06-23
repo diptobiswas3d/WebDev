12c90f9345c30781612e1ddda039f81ae2aff31c
diptobiswas3d <diptobiswasgpt4@gmail.com>
2023-06-24 00:28:05 +0530

    feat - adding elems, modifying text

--- JS/DOM/script.js
+++ JS/DOM/script.js
@@ -1,3 +1,13 @@
+const container = document.querySelector(".container")
+
+let divElem = document.createElement("div")
+divElem.setAttribute("class", "anime")
+divElem.setAttribute("id", "anime-5")
+divElem.textContent = "Bleach"
+container.append(divElem)
+
+console.log(divElem);
+
 // Gets the elem with unique id
 let naruto = document.getElementById("anime-1")
 console.log(naruto);
@@ -17,3 +27,8 @@ console.log(aot);
 // Gets an array of elems that matches the selector
 let animeBySelector = document.querySelectorAll(".anime")
 console.log(animeBySelector);
+
+// Modifying text
+naruto.innerHTML = "Pokemon"
+anime[1].innerText = "Digimon"
+aot.textContent = "Dragon Ball"
