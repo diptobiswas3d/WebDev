a9b5834a4d3dd77df628991f5350634d4a73e9f3
diptobiswas3d <diptobiswasgpt4@gmail.com>
2023-06-23 23:59:30 +0530

    feat - Adding JS, DOM

---
+++ JS/DOM/script.js
@@ -0,0 +1,19 @@
+// Gets the elem with unique id
+let naruto = document.getElementById("anime-1")
+console.log(naruto);
+
+// Gets an array of elems with the class name
+let anime = document.getElementsByClassName("anime")
+console.log(anime);
+
+// Gets an array of elems with the tag name
+let divTag = document.getElementsByTagName("div")
+console.log(divTag);
+
+// Gets the 1st elem that matches the selector
+let aot = document.querySelector("#anime-3")
+console.log(aot);
+
+// Gets an array of elems that matches the selector
+let animeBySelector = document.querySelectorAll(".anime")
+console.log(animeBySelector);
