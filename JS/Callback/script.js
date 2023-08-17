3c63b0015ee0d44035161a00cdf49e1cd2bb1af4
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-08-18 01:04:06 +0530

    feat - Flexbox

--- JS/Callback/script.js
+++ JS/Callback/script.js
@@ -12,9 +12,9 @@ function div(a, b) {
   console.log(a / b);
 }
 
-sum(x, y);
-mul(x, y);
-div(x, y);
+// sum(x, y);
+// mul(x, y);
+// div(x, y);
 
 function calc(a, b, callback) {
   callback(a, b);
@@ -27,3 +27,17 @@ calc(x, y, div);
 addEventListener("mousemove", (e) => {
   console.log(e.clientX, e.clientY);
 });
+
+// Callback Hell, Pyramid of Doom
+setTimeout(() => {
+  console.log("Alert 1");
+  setTimeout(() => {
+    console.log("Alert 2");
+    setTimeout(() => {
+      console.log("Alert 3");
+      setTimeout(() => {
+        console.log("Alert 4");
+      }, 4000);
+    }, 3000);
+  }, 2000);
+}, 1000);
