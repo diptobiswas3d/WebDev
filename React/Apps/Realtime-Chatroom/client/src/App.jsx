c2505cce480b499570a2fcdb744eb38652180eb1
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-08-23 10:03:22 +0530

    feat - user is typing update

--- React/Apps/Realtime-Chatroom/client/src/App.jsx
+++ React/Apps/Realtime-Chatroom/client/src/App.jsx
@@ -4,48 +4,135 @@ import "./App.css";
 
 const ENDPOINT = "http://localhost:3001";
 let socket;
+let pkmnTrainers = [
+  "Ash",
+  "Gary",
+  "Red",
+  "Dawn",
+  "Barry",
+  "Lyra",
+  "May",
+  "Misty",
+  "Brock",
+  "Steven",
+];
 
 function App() {
+  const [username, setUsername] = useState(
+    pkmnTrainers[Math.floor(Math.random() * 10)]
+  );
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
+  const [typingUser, setTypingUser] = useState("");
+  const [dotCount, setDotCount] = useState(1); // This will represent the number of dots.
 
   useEffect(() => {
+    const interval = setInterval(() => {
+      setDotCount((prevCount) => (prevCount % 3) + 1); // This will cycle the count between 1 and 3.
+    }, 500);
+
+    // Cleanup: Clear the interval when the component unmounts.
+    return () => clearInterval(interval);
+  }, []);
+  useEffect(() => {
+    // let randomUsername = pkmnTrainers[Math.floor(Math.random() * 10)];
+    // setUsername(randomUsername);
+    console.log(username, "joined the chat");
     socket = socketioclient(ENDPOINT);
+
+    socket.emit("new-user", username);
+
+    socket.on("user-typing", (user) => {
+      console.log(user, "is typing - received from server");
+      setTypingUser(user);
+    });
+    socket.emit("stop-typing", username);
+    socket.on("user-stop-typing", (user) => {
+      console.log(user, "stopped typing - received from server");
+      if (typingUser == user) setTypingUser("");
+    });
+
+    socket.on("user-connected", (user) => {
+      console.log(user, "joined the chat");
+      setChat((prevChat) => [
+        ...prevChat,
+        { message: `${user} joined the chat`, dir: "left" },
+      ]);
+      socket.on("user-disconnected", (user) => {
+        setChat((prevChat) => [
+          ...prevChat,
+          { message: `${user} left the chat`, dir: "left" },
+        ]);
+      });
+    });
+
     socket.on("chat-message", (data) => {
-      console.log(data);
-      setChat((prevChat) => [...prevChat, { message: data, dir: "left" }]);
-      console.log(chat);
+      console.log(data.msg);
+      setChat((prevChat) => [
+        ...prevChat,
+        { user: data.user, message: data.msg, dir: "left" },
+      ]);
+      // console.log(chat);
     });
     return () => {
       socket.off("chat-message");
+      socket.off("user-connected");
+      socket.off("user-disconnected");
+      socket.off("user-typing");
+      socket.off("user-stop-typing");
     };
     // socket.emit("send-chat-message", "Eevee use tackle");
   }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
-    setChat((prevChat) => [...prevChat, { message: message, dir: "right" }]);
-    console.log(message);
-    console.log(chat);
+    setChat((prevChat) => [
+      ...prevChat,
+      { user: "You", message: message, dir: "right" },
+    ]);
+    // console.log(message);
+    // console.log(chat);
     socket.emit("send-chat-message", message);
+    socket.emit("stop-typing", username);
+    console.log("Sending stop-typing to server after message sent");
+    setMessage("");
   }
 
   function writeMessage(e) {
-    console.log(message);
-    setMessage(e.target.value);
+    const curMessage = e.target.value;
+    // console.log(message);
+    setMessage(curMessage);
+    if (curMessage == "") {
+      socket.emit("stop-typing", username);
+      console.log("Sending stop-typing to server");
+    } else {
+      socket.emit("typing", username);
+      console.log("Sending typing to server");
+    }
+    console.log(`${username}: ${curMessage}`);
+    console.log("Typing user:", typingUser);
   }
 
   return (
     <main>
       <h1>Neo Chat App React</h1>
       <div id="message-container">
+        <div className="message right">You joined the chat</div>
         {chat.map((message, index) => {
           return (
             <div key={index} className={"message " + message.dir}>
-              {message.message}
+              {message.user
+                ? `${message.user}: ${message.message}`
+                : message.message}
             </div>
           );
         })}
+        {typingUser && (
+          <div className="message left">
+            {typingUser} is typing
+            {dotCount === 1 ? "." : dotCount === 2 ? ".." : "..."}
+          </div>
+        )}
       </div>
       <form id="send-container">
         <input
--- React/Apps/Realtime-Chatroom/server/server.js
+++ React/Apps/Realtime-Chatroom/server/server.js
@@ -4,11 +4,29 @@ const io = require("socket.io")(3001, {
   },
 });
 
+let users = {};
+
 io.on("connection", (socket) => {
   //   socket.emit("chat-message", "Let's fight a battle!");
 
+  socket.on("new-user", (user) => {
+    users[socket.id] = user;
+    socket.broadcast.emit("user-connected", user);
+  });
+
   socket.on("send-chat-message", (msg) => {
-    console.log(msg);
-    socket.broadcast.emit("chat-message", msg);
+    // console.log(`${users[socket.id]}: ${msg}`);
+    socket.broadcast.emit("chat-message", { user: users[socket.id], msg });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+  socket.on("typing", (user) => {
+    console.log(`${user} is typing`);
+    socket.broadcast.emit("user-typing", user);
+  });
+  socket.on("stop-typing", (user) => {
+    socket.broadcast.emit("user-stop-typing", user);
   });
 });
