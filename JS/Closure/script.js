4efb93eebd2a1f696d7c53a052fab5a541eec33e
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-08-28 21:59:10 +0530

    feat - Python

--- JS/Closure/script.js
+++ JS/Closure/script.js
@@ -1,13 +1,29 @@
-let url = "https://pokeapi.co/api/v2";
-let query = "/pokemon";
-let name = "/pikachu";
+// let normal_var = "I'm a normal var";
 
-let endpoint = url + query + name;
+// // Closure
+// function normal_fn() {
+//   console.log(normal_var);
+// }
 
-function getData() {
-  fetch(endpoint)
+// normal_fn();
+
+// // Complex
+// function outer_fn(outer_var) {
+//   inner_fn("Inner var");
+//   function inner_fn(inner_var) {
+//     console.log(outer_var);
+//     console.log(inner_var);
+//   }
+// }
+
+// outer_fn("Outer var");
+
+let url = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
+function getPkmnData() {
+  fetch(url)
     .then((res) => res.json())
     .then((data) => console.log(data));
 }
 
-getData();
+getPkmnData();
